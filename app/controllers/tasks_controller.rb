05ebfb310c9d92bc1cdda8b4e3adeb063378a69e
Rose Grondin <rose.grondin@gmail.com>
2020-02-18 13:18:52 -0500

    add a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,14 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    safe_params = params.require(:task).permit(:title, :details)
+    task = Task.new(safe_params)
+    task.save
+  end
 end
