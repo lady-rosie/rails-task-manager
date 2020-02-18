c9a143307294a881ecf375222796d67f42ad629d
Rose Grondin <rose.grondin@gmail.com>
2020-02-18 15:15:28 -0500

    add the destroy action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,5 +15,23 @@ class TasksController < ApplicationController
     safe_params = params.require(:task).permit(:title, :details)
     task = Task.new(safe_params)
     task.save
+    redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    safe_params = params.require(:task).permit(:title, :details, :completed)
+    @task.update(safe_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   get '/tasks', to: 'tasks#index'
   get '/tasks/new', to: 'tasks#new', as: 'new_task'
   get '/tasks/:id', to: 'tasks#show', as: 'task'
+  patch '/tasks/:id', to: 'tasks#update'
+  delete '/tasks/:id', to: 'tasks#destroy'
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
   post '/tasks', to: 'tasks#create'
 end
